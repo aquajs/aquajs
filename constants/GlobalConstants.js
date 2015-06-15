b6644a123fad9e10851e67c7410651b21ff3c8d4
aatibudhi <Chakradhar1>
2015-06-15 15:59:18 -0700

    for the logger new methods addition

--- constants/GlobalConstants.js
+++ constants/GlobalConstants.js
@@ -2,17 +2,17 @@ var path = require('path');
 
 //$dirPaths,$config,$app are used in bootstrap
 GLOBAL.$dirPaths = require(path.join(process.cwd(), 'config', 'dirPaths'));
-GLOBAL.$config;
-GLOBAL.$app;//Used in bootstrap and connUtils
-GLOBAL.$logger;// instantiated in bootstrap and used across the application .= AquaJsLogger.getLogger();
-GLOBAL.$winston;// instantiated in bootstrap= AquaJsLogger.getWinston();
+GLOBAL.$config = {};
+GLOBAL.$app = {};//Used in bootstrap and connUtils
+GLOBAL.$logger = {} ;// instantiated in bootstrap and used across the application .= AquaJsLogger.getLogger();
+GLOBAL.$winston = {};// instantiated in bootstrap= AquaJsLogger.getWinston();
 
 //enableWaterline,enablePersist are used  in bootstrap and conn Utils
-GLOBAL.$enableWaterline;
-GLOBAL.$enablePersist;
+GLOBAL.$enableWaterline = false;
+GLOBAL.$enablePersist = false;
 GLOBAL.$appId = "[aquajs-logger]";
 
 //$initModel ,$conn used in connUtils
 GLOBAL.$initModel = false;
-GLOBAL.$conn;
+GLOBAL.$conn = {};
 
